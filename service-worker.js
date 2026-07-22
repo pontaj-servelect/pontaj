105d54a688bbd83480a83e5b359a9ea542735e51
pontaj-servelect <ionut.lazar@servelect.ro>
2026-07-22 13:07:22 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const BUILD='offline-first-v4.9.27-servio-direct-jsonp-write-20260722-1';
+const BUILD='offline-first-v4.9.28-servio-stable-post-restore-notifications-20260722-1';
 const CACHE_PREFIX='servelect-pontaj-';
 const CACHE_NAME=CACHE_PREFIX+BUILD;
 const CORE=[
